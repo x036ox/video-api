a45fddfb1d35cb69fd01c036f34aa0fcb2f5ea2c
Artur <arttturyyy@gmail.com>
2024-10-07 20:57:44 +0200

    config fix

--- src/main/java/com/artur/youtback/config/SecurityConfig.java
+++ src/main/java/com/artur/youtback/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.artur.youtback.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
